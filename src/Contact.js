b186873ffa57bc06dc81d2c5e70700cfdf500659
Ernest-hub <55037492+Ernest-hub@users.noreply.github.com>
2020-11-05 03:33:55 -0500

    untracked node_modules

--- src/Contact.js
+++ src/Contact.js
@@ -1,7 +1,32 @@
 import React from "react";
 
 const Contact = () => {
-	return <h3>This is Contact page</h3>
+	 return <div>
+	<h3>This is Contact page</h3> <br />
+
+	<form style={{
+
+		backgroundImage: 'url("https://webcomicms.net/sites/default/files/clipart/144257/animated-computer-pictures-144257-637982.gif")',
+		maxWidth: '30em',
+		backgroundRepeat: 'no-repeat',
+
+	}} >
+<hr />
+	FirstName :  <input type="" /> <br /> <br /> 
+	LastName : <input type="" /> <br /> <br />
+	LastName : <input type="" /> <br /> <br />
+	Password : <input type="password" /> <br /> <br />
+	LastName : <input type="" /> <br /> <br />
+	LastName : <input type="" /> <br /> <br />
+	LastName : <input type="" /> <br /> <br /> <hr />
+	<button> Enter </button> 
+
+	</form> 
+
+	</div>
+	
 }
+
+
 
 export default Contact;
--- src/FastTrackersList.js
+++ src/FastTrackersList.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const FastTrackersList = () => {
-	return <h3>This is FastTrackersList page</h3>
+	return <div>
+				<u><h3>This is FastTrackersList page</h3></u>
+				<ol> 
+					<li> Ajoa Mansah</li>
+					<li> John Smith</li>
+					<li> Apple Johnson</li>
+					<li> Babbob Alfred</li>
+					<li> Abby Aquah</li>
+					<li> Ajoa Mansah</li>
+					
+				</ol>
+			</div>
 
 
 
